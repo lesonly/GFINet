08aad65878217ada40649784c966cd48e110ca84
lesonly <58156801+lesonly@users.noreply.github.com>
2022-03-06 14:19:47 +0800

    Update mask_region.py

--- data2/mask_region.py
+++ data2/mask_region.py
@@ -15,17 +15,14 @@ def erosion_img(img,a,iterations):
 def nothing(pos):
     pass
 
-with open('E:\\GFINet-master\\data\\DUTS\\train.txt', 'r') as lines:
+with open('./data/DUTS/train.txt', 'r') as lines:
     samples = []
     for line in lines:
-        maskpath  = 'E:\\GFINet-master\\data\\DUTS\\mask\\'  + line.strip() + '.png'
-   
+        maskpath  = './data/DUTS/mask/'  + line.strip() + '.png'   
         img = cv2.imread(maskpath,1)
-
-
         dilated = dilate_img(img,3,5)
         erosion = erosion_img(img, 3,5)
         is1 = dilated-img
         is2=img-erosion
         intersec=is1+is2
-        cv2.imwrite('E:\\GFINet-master\\data\\DUTS\\mask_region\\'+ line.strip() + '.png',intersec)
+        cv2.imwrite('./data/DUTS/mask_region/'+ line.strip() + '.png',intersec)
